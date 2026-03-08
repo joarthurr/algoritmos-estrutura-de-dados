4b45ee3629090ae27d23411bc9cb41730f2882fb
heberthysamir <heberthy.samir@aluno.ufca.edu.br>
2026-03-08 18:45:09 -0300

    Geração-Dados

--- Ocorrencia/main.c
+++ Ocorrencia/main.c
@@ -1,55 +1,107 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 #include "lista_estatica.h"
 #include "lista_dinamica.h"
 #include "ocorrencia.h"
 
-void testarListaEstatica() {
-    ListaEstatica lista;
-    inicializar_estatica(&lista);
+/* ===== FUNÇÕES DE GERAÇÃO DE DADOS ===== */
 
-    Ocorrencia o1 = {1, 5, 3.2, 10};
-    Ocorrencia o2 = {2, 2, 1.5, 5};
-    Ocorrencia o3 = {3, 4, 2.8, 7};
-
-    inserir_estatica(&lista, o1);
-    inserir_estatica(&lista, o2);
-    inserir_estatica(&lista, o3);
-
-    imprimir_estatica(&lista);
-
-    printf("\nRemovendo a Ocorrencia de ID 2\n");
-    remover_estatica(&lista, 2);
-
-    imprimir_estatica(&lista);
+// Aleatória
+Ocorrencia gerarOcorrencia(int id) {
+    Ocorrencia o;
+    o.id = id;
+    o.prioridade = rand() % 5 + 1;
+    o.distancia = (float)(rand() % 2000) / 100; // 0.0 a 20.0
+    o.tempoEspera = rand() % 120;
+    return o;
 }
 
-void testarListaDinamica() {
-    NoDinamico *lista = NULL;
-
-    Ocorrencia o1 = {1, 5, 3.2, 10};
-    Ocorrencia o2 = {2, 2, 1.5, 5};
-    Ocorrencia o3 = {3, 4, 2.8, 7};
-
-    inserir_dinamica(&lista, o1);
-    inserir_dinamica(&lista, o2);
-    inserir_dinamica(&lista, o3);
-
-    imprimir_dinamica(lista);
-
-    printf("\nRemovendo a Ocorrencia de ID 2\n");
-    remover_dinamica(&lista, 2);
-
-    imprimir_dinamica(lista);
-    liberar_dinamica(lista);
+// Inverson (pior caso) 
+Ocorrencia gerarOcorrenciaInversa(int id, int n) {
+    Ocorrencia o;
+    o.id = id;
+    o.prioridade = 5 - ((id - 1) * 5 / n); 
+    if(o.prioridade < 1) o.prioridade = 1;
+    o.distancia = id * 0.1f;
+    o.tempoEspera = n - id;
+    return o;
 }
+
+// Ordenada
+Ocorrencia gerarOcorrenciaOrdenada(int id, int n) {
+    Ocorrencia o;
+    o.id = id;
+    o.prioridade = 1 + ((id - 1) * 5 / n);
+    if(o.prioridade > 5) o.prioridade = 5;
+    o.distancia = (n - id) * 0.1f;
+    o.tempoEspera = id;
+    return o;
+}
+
+/* ===== MAIN ===== */
 
 int main() {
+    srand(time(NULL));
 
-    printf("Testando Lista Estatica:\n");
-    testarListaEstatica();
+    int n, tipo, estrutura;
+    
+    printf("--- Simulador de Ocorrencias ---\n");
+    printf("Escolha a estrutura:\n 1-Lista Estatica\n 2-Lista Dinamica\nOpcao: ");
+    scanf("%d", &estrutura);
 
-    printf("\nTestando Lista Dinamica:\n");
-    testarListaDinamica();
+    printf("Digite a quantidade de dados: ");
+    scanf("%d", &n);
+
+    printf("Tipo de geracao:\n 0-Aleatoria\n 1-Inversa\n 2-Ordenada\nOpcao: ");
+    scanf("%d", &tipo);
+
+    if (estrutura == 1) {
+        // --- TESTE ESTÁTICO ---
+        ListaEstatica le;
+        inicializar_estatica(&le);
+
+        for (int i = 0; i < n; i++) {
+            Ocorrencia o;
+            int atualID = i + 1;
+
+            if (tipo == 0) o = gerarOcorrencia(atualID);
+            else if (tipo == 1) o = gerarOcorrenciaInversa(atualID, n);
+            else o = gerarOcorrenciaOrdenada(atualID, n);
+
+            inserir_estatica(&le, o);
+        }
+        printf("\n--- Lista Estatica Gerada ---\n");
+        imprimir_estatica(&le);
+        
+        printf("\nRemovendo ID 5 (se existir)...\n");
+        remover_estatica(&le, 5);
+        imprimir_estatica(&le);
+
+    } else {
+        // --- TESTE DINÂMICO ---
+        NoDinamico *ld = NULL;
+
+        for (int i = 0; i < n; i++) {
+            Ocorrencia o;
+            int atualID = i + 1;
+
+            if (tipo == 0) o = gerarOcorrencia(atualID);
+            else if (tipo == 1) o = gerarOcorrenciaOrdenada(atualID, n);
+            else o = gerarOcorrenciaInversa(atualID, n);
+            
+            inserir_dinamica(&ld, o);
+        }
+        printf("\n--- Lista Dinamica Gerada ---\n");
+        imprimir_dinamica(ld);
+        
+        printf("\nRemovendo ID 5 (se existir)...\n");
+        remover_dinamica(&ld, 5);
+        imprimir_dinamica(ld);
+        
+        liberar_dinamica(ld);
+    }
 
     return 0;
 }
